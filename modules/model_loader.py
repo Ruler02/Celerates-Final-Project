05529079ce4637e54be25eacc26e0c2997b4459b
Ruler02 <khaidarrasyad@gmail.com>
2025-12-10 07:45:10 +0700

    Refactor load_dataset function to use Path for cross-platform compatibility and add file existence check

--- modules/model_loader.py
+++ modules/model_loader.py
@@ -1,10 +1,18 @@
 import pickle
 import pandas as pd
+from pathlib import Path
 
 def load_model():
     with open("models/best_model.pkl", "rb") as f:
         return pickle.load(f)
 
 def load_dataset():
-    df = pd.read_csv("data\BreastCancer123.csv")
+    # Path otomatis menyesuaikan Windows/Linux
+    file_path = Path(__file__).parent.parent / "data" / "BreastCancer123.csv"
+
+    # Cek file ada atau tidak → lebih mudah debug
+    if not file_path.exists():
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    df = pd.read_csv(file_path)
     return df.drop(columns=["diagnosis", "Unnamed: 32", "id"])
