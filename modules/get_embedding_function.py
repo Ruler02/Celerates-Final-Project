6bd3ac992406b6bc1c079f6c309225f41078c94d
Ruler02 <khaidarrasyad@gmail.com>
2025-12-10 19:37:53 +0700

    Refactor get_embedding_function to include SentenceTransformer model initialization for improved embedding handling

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,16 +1,19 @@
 import os
 from langchain.embeddings import HuggingFaceHubEmbeddings  # resmi
+from sentence_transformers import SentenceTransformer
 
 def get_embedding_function():
     """
     Ambil HuggingFace embeddings yang aman di Streamlit Cloud.
     API key diambil dari os.environ["HUGGINGFACEHUB_API_TOKEN"].
     """
+    
     hf_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN", None)
     if hf_token is None:
         raise ValueError("HUGGINGFACEHUB_API_TOKEN tidak ditemukan! Masukkan API key di app.py atau Streamlit Secrets.")
 
     embeddings = HuggingFaceHubEmbeddings(
+        model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device="cpu")
         repo_id="sentence-transformers/all-MiniLM-L6-v2",  # model resmi HF
         huggingfacehub_api_token=hf_token,
         model_kwargs={"device": "cpu"}  # paksa CPU
