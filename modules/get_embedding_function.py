a916e03e8b02f0082f5269b9e7a2cb27895507ac
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 20:18:26 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,8 +1,9 @@
-from langchain.embeddings import HuggingFaceHubEmbeddings
+# modules/get_embedding_function.py
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embedding_function(api_key):
-    embeddings = HuggingFaceHubEmbeddings(
-        repo_id="sentence-transformers/all-mpnet-base-v2",
+    embeddings = HuggingFaceEmbeddings(
+        model="sentence-transformers/all-mpnet-base-v2",
         huggingfacehub_api_token=api_key
     )
     return embeddings
