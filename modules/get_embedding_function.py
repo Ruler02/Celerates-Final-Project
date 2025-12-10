7ad11b0829b72870b657d098d8ed6d00c0e44248
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 21:38:27 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,20 +1,21 @@
-from langchain_huggingface import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 import os
 
 def get_embedding_function(api_key=None):
     """
-    Mengembalikan embedding function menggunakan HuggingFace BGE Embedding.
-    Jika model private → wajib pakai API Key.
+    Embedding function menggunakan HuggingFace Sentence-Transformer
+    untuk integrasi dengan ChromaDB.
     """
-    if api_key:
+
+    if api_key is not None:
         os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_key
 
-    # Model embedding ringan & akurat untuk RAG
     model_name = "sentence-transformers/all-mpnet-base-v2"
 
-    embedding_model = HuggingFaceBgeEmbeddings(
-        model_name="sentence-transformers/all-mpnet-base-v2",
+    embedding = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs={"device": "cpu"},
         encode_kwargs={"normalize_embeddings": True},
     )
 
-    return embedding_model
+    return embedding
