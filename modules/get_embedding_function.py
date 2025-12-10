1429bb9756365a1914d5c54519420ab1e4616348
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 20:14:54 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,9 +1,6 @@
 from langchain.embeddings import HuggingFaceHubEmbeddings
 
-def get_embedding_function(api_key: str):
-    """
-    Membuat HuggingFaceHubEmbeddings untuk Streamlit Cloud.
-    """
+def get_embedding_function(api_key):
     embeddings = HuggingFaceHubEmbeddings(
         repo_id="sentence-transformers/all-mpnet-base-v2",
         huggingfacehub_api_token=api_key
