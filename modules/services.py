c12dd56ec26c8f3753dbf2c47c89b0266f79ceaf
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 20:16:20 +0700

    Update services.py

--- modules/services.py
+++ modules/services.py
@@ -8,10 +8,10 @@ def query_rag(query_text, chroma_path, diagnosis, benign_template, malignant_tem
     """
     Melakukan pencarian RAG dan generate jawaban menggunakan Hugging Face endpoint.
     """
+    embedding_function = get_embedding_function(api_key)
+    
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_key
 
-    # Ambil embedding function dengan api_key
-    embedding_function = get_embedding_function(api_key)
     
     # Load vectorstore Chroma
     db = Chroma(persist_directory=chroma_path, embedding_function=embedding_function)
