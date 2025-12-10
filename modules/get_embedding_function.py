7964df154f869917c9e3049dc3752a6f5015a39d
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 21:35:49 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,8 +1,20 @@
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceBgeEmbeddings
+import os
 
-def get_embedding_function():
-    (
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-    
-    return model_name
+def get_embedding_function(api_key=None):
+    """
+    Mengembalikan embedding function menggunakan HuggingFace BGE Embedding.
+    Jika model private → wajib pakai API Key.
+    """
+    if api_key:
+        os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_key
+
+    # Model embedding ringan & akurat untuk RAG
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+
+    embedding_model = HuggingFaceBgeEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2",
+        encode_kwargs={"normalize_embeddings": True},
     )
+
+    return embedding_model
