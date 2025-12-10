389372637e5e7566d22f6157f0e5667a39891516
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 21:29:23 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,6 +1,8 @@
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
 def get_embedding_function():
-    return HuggingFaceEmbeddings(
+    (
         model_name="sentence-transformers/all-MiniLM-L6-v2",
+    
+    return model_name
     )
