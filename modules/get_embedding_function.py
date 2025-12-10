c4944ed81b4c7ffdf92c9c79fb00001e29905118
Ruler02 <khaidarrasyad@gmail.com>
2025-12-10 19:40:55 +0700

    Refactor get_embedding_function to remove unused environment variable for HuggingFace API token

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,10 +1,8 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
-import os
 
 def get_embedding_function(api_key):
     embeddings = HuggingFaceEmbeddings(
         model="sentence-transformers/all-mpnet-base-v2",
-        hf_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
     )
     return embeddings
