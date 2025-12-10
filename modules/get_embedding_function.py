21c22088e0f08d9855145349e9fed8aaafd4c240
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-10 21:49:40 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,22 +1,14 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
-import os
 
 def get_embedding_function(api_key=None):
-    """
-    Embedding function menggunakan HuggingFace Sentence-Transformer
-    untuk integrasi dengan ChromaDB.
-    """
+    model_name = "sentence-transformers/paraphrase-MiniLM-L6-v2"
 
-    if api_key is not None:
-        os.environ["HUGGINGFACEHUB_API_TOKEN"] = api_key
-    
     embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-mpnet-base-v2",
-    model_kwargs={
-        "device": "cpu",
-        "trust_remote_code": True
-        "local_files_only": True  # penting agar tak meta tensor
-    },
-    encode_kwargs={"normalize_embeddings": True},
+        model_name=model_name,
+        model_kwargs={
+            "device": "cpu",
+        },
+        encode_kwargs={"normalize_embeddings": True}
     )
+    
     return embedding
