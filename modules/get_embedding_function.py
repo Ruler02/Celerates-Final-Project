6110b850c2d3ae81a36ed978238f2184abca991a
Ruler02 <khaidarrasyad@gmail.com>
2025-12-10 19:39:07 +0700

    Refactor get_embedding_function to use HuggingFaceEmbeddings for improved embedding handling and remove deprecated code

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,22 +1,10 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 import os
-from langchain.embeddings import HuggingFaceHubEmbeddings  # resmi
-from sentence_transformers import SentenceTransformer
 
-def get_embedding_function():
-    """
-    Ambil HuggingFace embeddings yang aman di Streamlit Cloud.
-    API key diambil dari os.environ["HUGGINGFACEHUB_API_TOKEN"].
-    """
-    
-    hf_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN", None)
-    if hf_token is None:
-        raise ValueError("HUGGINGFACEHUB_API_TOKEN tidak ditemukan! Masukkan API key di app.py atau Streamlit Secrets.")
-
-    embeddings = HuggingFaceHubEmbeddings(
-        model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device="cpu")
-        repo_id="sentence-transformers/all-MiniLM-L6-v2",  # model resmi HF
-        huggingfacehub_api_token=hf_token,
-        model_kwargs={"device": "cpu"}  # paksa CPU
+def get_embedding_function(api_key):
+    embeddings = HuggingFaceEmbeddings(
+        model="sentence-transformers/all-mpnet-base-v2",
+        hf_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
     )
-
     return embeddings
