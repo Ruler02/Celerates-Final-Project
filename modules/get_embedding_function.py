01f228bdedc645a811acbab2bee865e342172976
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 08:07:42 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,22 +1,9 @@
-import streamlit as st
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+import streamlit as st
 
 def get_embedding_function():
     return HuggingFaceInferenceAPIEmbeddings(
         api_key=st.secrets["HF_APIKEY"],
         model_name="BAAI/bge-base-en-v1.5",
+        api_url="https://router.huggingface.co"
     )
-
-import requests
-
-API_KEY = st.secrets["HF_APIKEY"]
-MODEL = "BAAI/bge-base-en-v1.5"
-
-response = requests.post(
-    f"https://api-inference.huggingface.co/models/{MODEL}",
-    headers={"Authorization": f"Bearer {API_KEY}"},
-    json={"inputs": "test embedding"}
-)
-
-st.write(response.json())
-
