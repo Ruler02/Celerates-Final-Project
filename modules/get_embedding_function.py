87ff37c70f12ff424f337a86d72c5f047ee6b006
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 08:11:34 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -4,6 +4,6 @@ import streamlit as st
 def get_embedding_function():
     return HuggingFaceInferenceAPIEmbeddings(
         api_key=st.secrets["HF_APIKEY"],
-        model_name="BAAI/bge-base-en-v1.5",
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
         api_url="https://router.huggingface.co"
     )
