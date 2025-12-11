727a15c0dee1d01b47818db873a9e2ebeaf1b2e3
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 07:57:08 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 
-st.write("APINYA:", st.secrets["HF_APIKEY"])
-
 def get_embedding_function():
     return HuggingFaceInferenceAPIEmbeddings(
         api_key=st.secrets["HF_APIKEY"],
-        model_name="BAAI/bge-base-en-v1.5"
+        model_name="BAAI/bge-base-en-v1.5",
+        task="feature-extraction" 
     )
