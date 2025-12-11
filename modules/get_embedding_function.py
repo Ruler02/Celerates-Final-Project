5913e251db3e1ab8dfca2e718d5420a0166cb8ef
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 08:29:33 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -9,6 +9,7 @@ def get_embedding_function():
     Hanya load sekali per session
     """
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
     )
-
