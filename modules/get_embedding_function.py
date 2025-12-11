8e76c4e121e9d1f5ebae8aa48cf0573cb517945e
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 07:36:18 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,8 +1,13 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import streamlit as st
 
-def get_embedding_function(api_key=None):
+
+def get_embedding_function():
+    hf_key = st.secrets["HF_APIKEY"]
+
     return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
         model_kwargs={"device": "cpu"},
         encode_kwargs={"normalize_embeddings": True},
+        api_key=hf_key,
     )
