a2511eb9b99a134ac768899db548bc1c5a63efd1
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 07:59:40 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -5,7 +5,6 @@ def get_embedding_function():
     return HuggingFaceInferenceAPIEmbeddings(
         api_key=st.secrets["HF_APIKEY"],
         model_name="BAAI/bge-base-en-v1.5",
-        task="feature-extraction" 
     )
 
 import requests
