4ebb126d314763c8b93a2da9af7ad8c564dcf967
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 07:50:51 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,8 +1,8 @@
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 import streamlit as st
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 
 def get_embedding_function():
     return HuggingFaceInferenceAPIEmbeddings(
         api_key=st.secrets["HF_APIKEY"],
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="BAAI/bge-m3"
     )
