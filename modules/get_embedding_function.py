0384d2122f0b2a0ae8a7acf6552f1d8a42f987d6
Ruler02 <142371518+Ruler02@users.noreply.github.com>
2025-12-11 07:40:48 +0700

    Update get_embedding_function.py

--- modules/get_embedding_function.py
+++ modules/get_embedding_function.py
@@ -1,9 +1,6 @@
-import streamlit as st
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
+from langchain.embeddings import SentenceTransformerEmbeddings
 
 def get_embedding_function():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True},
-    )
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return SentenceTransformerEmbeddings(model)
